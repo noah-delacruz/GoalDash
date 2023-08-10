4a96f88da88c9c925d5c15c090327855854d98a2
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-08-09 19:25:16 -0700

    Add controllers for routes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-    res.json({message: 'Get goals'})
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
