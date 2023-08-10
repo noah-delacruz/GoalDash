fa61f51156132aba36a1d723f88df10367b42df4
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-08-09 19:33:03 -0700

    Add error and exception handling

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,17 +9,22 @@ const getGoals = (req, res) => {
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    console.log(req.body)
     res.status(200).json({message: 'Set goal'})
 }
 
-// @desc    Update goals
+// @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
 }
 
-// @desc    Delete goals
+// @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = (req, res) => {
