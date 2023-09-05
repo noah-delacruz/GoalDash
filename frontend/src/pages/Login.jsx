edf01b70a0e578865ececda930cd63e74bd5b022
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-09-04 20:40:49 -0700

    Make forms for login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { FaSign, FaSignInAlt } from 'react-icons/fa';
 
 export default function Login() {
+    const [formData, setFormData] = React.useState({
+        email: '',
+        password: '',
+    })
+
+    const {email, password} = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt /> Login
+                </h1>
+                <p>Login and start setting goals</p>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input type='email' className='form-control' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter password' onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <button type='submit' className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
