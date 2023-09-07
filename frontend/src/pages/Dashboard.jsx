7e1e93f64a2937191738c7a0cda38a6ef4c9e3a1
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-09-06 20:37:52 -0700

    Setup dashboard & goal form

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 export default function Dashboard() {
     const navigate = useNavigate()
@@ -19,6 +20,8 @@ export default function Dashboard() {
                 <h1>Welcome {user && user.name}</h1>
                 <p>Goals Dashboard</p>
             </section>
+
+            <GoalForm />
         </>
     )
 }
